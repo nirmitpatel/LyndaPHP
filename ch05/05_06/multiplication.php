62871c9833784b65ae7a98e03f3b6cf8e05b12cd
nirmitp <nirmitpatel1994@gmail.com>
2017-01-20 14:28:41 -0800

    Chapter 5

--- ch05/05_06/multiplication.php
+++ ch05/05_06/multiplication.php
@@ -8,7 +8,29 @@
 <body>
 <h1>Multiplication Table</h1>
 <table>
+    <?php
+        echo '<tr>';
+            echo '<th>&nbsp;</th>';
+            for ($col = 1; $col <= 12; $col++):
+                echo "<th>$col</th>";
+            endfor;
+        echo '</tr>';
 
+        for ($row = 1, $col = 1; $row <= 12; $row++):
+            echo '<tr>';
+            if ($col = 1)
+                echo "<th>$row</th>";
+
+            while ($col >= 1 && $col <= 12):
+                $mul = $row*$col;
+                echo "<td>$mul</td>";
+                $col++;
+            endwhile;
+
+            echo '</tr>';
+        endfor;
+
+    ?>
 </table>
 </body>
 </html>
